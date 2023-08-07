df5f52f5afd5610ab6fbd33f8dc2ed7ff8291a7a
Fernando Amores <60133103+0utsIdEw0rld@users.noreply.github.com>
2023-08-07 13:14:59 +0200

    Create iq_dynamic_execute.sql

---
+++ sql/iq_dynamic_execute.sql
@@ -0,0 +1,12 @@
+/* Example proc that create a temp table with a dynamic value */
+create or replace proc dbo.sp_bis_dba_dynamic_execute (
+    @num_value int
+)
+as
+begin
+    -- Dynamically create the temp table with the requested column value
+    execute('select ' || @num_value || ' as column_1 into #temp')
+    -- Return the result
+    select column_1 from #temp
+end;
+exec dbo.sp_bis_dba_dynamic_execute 123
